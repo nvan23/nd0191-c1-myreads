cd018b369152250ecb120c6a0c403579e6884ac2
nvan23 <dtnvan230898@gmail.com>
2024-08-11 15:08:13 +0700

    feat: split legacy code into components and apply routing

---
+++ starter/src/components/BookSeacher.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
+
+function BookSearcher() {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, author, or ISBN" />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+}
+
+export default BookSearcher;
